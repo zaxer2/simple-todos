35644cab0a77d6b3f0988299aaf3897f3962c5f4
zaxer2 <william.johns3@gmail.com>
2018-10-14 11:58:19 -1000

    Section 2: crashing
    I cannot proceed in the tutorial. App is crashing. I'll come back to this later if I have time in the day.

--- app/imports/ui/App.js
+++ app/imports/ui/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
 
+import { Tasks } from '../api/tasks.js';
 import Task from './Task.js';
 
 // App component - represents the whole app
-export default class App extends Component {
+class App extends Component {
   getTasks() {
     return [
       { _id: 1, text: 'This is task 1' },
@@ -13,7 +15,7 @@ export default class App extends Component {
   }
 
   renderTasks() {
-    return this.getTasks().map((task) => (
+    return this.props.tasks.map((task) => (
         <Task key={task._id} task={task} />
   ));
   }
@@ -32,3 +34,9 @@ export default class App extends Component {
   );
   }
 }
+
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}).fetch(),
+  };
+})(App);
